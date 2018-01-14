e8f33e761510787b5714191c5980b00a184b2bfe
oleggator <oleg@utkin.xyz>
2018-01-14 17:09:36 +0300

    Change iris to fasthttp

--- main.go
+++ main.go
@@ -2,44 +2,45 @@ package main
 
 import (
 	"github.com/jackc/pgx"
-	"github.com/kataras/iris"
 	"github.com/oleggator/tp-db/db"
 	"github.com/oleggator/tp-db/handlers"
+
+	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
 )
 
 func main() {
-	dbConfig := pgx.ConnConfig{
+	db.InitDB(pgx.ConnConfig{
 		Host:     "localhost",
 		Port:     5432,
 		Database: "docker",
 		User:     "docker",
 		Password: "docker",
-	}
-	db.InitDB(dbConfig)
+	})
 
-	app := iris.New()
+	router := fasthttprouter.New()
 
-	app.Post("/api/forum/create", handlers.ForumCreatePost)
-	app.Post("/api/forum/{slug:string}/create", handlers.ForumSlugCreatePost)
-	app.Get("/api/forum/{slug:string}/details", handlers.ForumSlugDetailsGet)
-	app.Get("/api/forum/{slug:string}/threads", handlers.ForumSlugThreadsGet)
-	app.Get("/api/forum/{slug:string}/users", handlers.ForumSlugUsersGet)
+	router.POST("/api/forum/:slug", handlers.ForumCreatePost) // "/api/forum/create"
+	router.POST("/api/forum/:slug/create", handlers.ForumSlugCreatePost)
+	router.GET("/api/forum/:slug/details", handlers.ForumSlugDetailsGet)
+	router.GET("/api/forum/:slug/threads", handlers.ForumSlugThreadsGet)
+	router.GET("/api/forum/:slug/users", handlers.ForumSlugUsersGet)
 
-	app.Get("/api/post/{id:int}/details", handlers.PostIdDetailsGet)
-	app.Post("/api/post/{id:int}/details", handlers.PostIdDetailsPost)
+	router.GET("/api/post/:id/details", handlers.PostIdDetailsGet)
+	router.POST("/api/post/:id/details", handlers.PostIdDetailsPost)
 
-	app.Get("/api/service/status", handlers.ServiceStatusGet)
-	app.Post("/api/service/clear", handlers.ServiceClearPost)
+	router.GET("/api/service/status", handlers.ServiceStatusGet)
+	router.POST("/api/service/clear", handlers.ServiceClearPost)
 
-	app.Post("/api/thread/{slug_or_id:string}/create", handlers.ThreadSlugOrIdCreatePost)
-	app.Get("/api/thread/{slug_or_id:string}/details", handlers.ThreadSlugOrIdDetailsGet)
-	app.Post("/api/thread/{slug_or_id:string}/details", handlers.ThreadSlugOrIdDetailsPost)
-	app.Get("/api/thread/{slug_or_id:string}/posts", handlers.ThreadSlugOrIdPostsGet)
-	app.Post("/api/thread/{slug_or_id:string}/vote", handlers.ThreadSlugOrIdVotePost)
+	router.POST("/api/thread/:slug_or_id/create", handlers.ThreadSlugOrIdCreatePost)
+	router.GET("/api/thread/:slug_or_id/details", handlers.ThreadSlugOrIdDetailsGet)
+	router.POST("/api/thread/:slug_or_id/details", handlers.ThreadSlugOrIdDetailsPost)
+	router.GET("/api/thread/:slug_or_id/posts", handlers.ThreadSlugOrIdPostsGet)
+	router.POST("/api/thread/:slug_or_id/vote", handlers.ThreadSlugOrIdVotePost)
 
-	app.Post("/api/user/{nickname:string}/create", handlers.UserNicknameCreatePost)
-	app.Get("/api/user/{nickname:string}/profile", handlers.UserNicknameProfileGet)
-	app.Post("/api/user/{nickname:string}/profile", handlers.UserNicknameProfilePost)
+	router.POST("/api/user/:nickname/create", handlers.UserNicknameCreatePost)
+	router.GET("/api/user/:nickname/profile", handlers.UserNicknameProfileGet)
+	router.POST("/api/user/:nickname/profile", handlers.UserNicknameProfilePost)
 
-	app.Run(iris.Addr(":5000"))
+	fasthttp.ListenAndServe(":5000", router.Handler)
 }
